45ab9978fbbd68019dbb9bf696d9d8bb159f2620
Parth <parthpatkiezio@gmail.com>
2024-11-28 19:03:32 +0530

    Rubocop fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  skip_before_action :require_authentication, only: [:new, :create]
+  before_action :set_user, only: [ :edit, :update, :show ]
+  skip_before_action :require_authentication, only: [ :new, :create ]
 
   def index
     @users = User.all
