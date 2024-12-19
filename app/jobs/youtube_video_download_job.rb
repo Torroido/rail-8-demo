b39b3ae576de02451e3c11afa725795a229c324d
Parth <parthpatkiezio@gmail.com>
2024-12-19 11:37:59 +0530

    Fixed linter

--- app/jobs/youtube_video_download_job.rb
+++ app/jobs/youtube_video_download_job.rb
@@ -2,7 +2,7 @@ class YoutubeVideoDownloadJob < ApplicationJob
   def perform(youtube_video_id)
     youtube_video = YoutubeVideo.find(youtube_video_id)
     link = youtube_video.url
-    url = URI("#{ENV['DOWNLOAD_URL']}/youtube-import") 
+    url = URI("#{ENV['DOWNLOAD_URL']}/youtube-import")
     headers = { "Content-Type" => "application/json" }
     body = { youtube_url: link }
     response = HTTParty.post(url, headers: headers, body: body.to_json).body
