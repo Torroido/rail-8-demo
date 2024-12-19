66cced1804668806db01769525712b761e7747b6
Parth <parthpatkiezio@gmail.com>
2024-12-19 11:34:00 +0530

    Added jobs and views

---
+++ app/jobs/youtube_video_download_job.rb
@@ -0,0 +1,19 @@
+class YoutubeVideoDownloadJob < ApplicationJob
+  def perform(youtube_video_id)
+    youtube_video = YoutubeVideo.find(youtube_video_id)
+    link = youtube_video.url
+    url = URI("#{ENV['DOWNLOAD_URL']}/youtube-import") 
+    headers = { "Content-Type" => "application/json" }
+    body = { youtube_url: link }
+    response = HTTParty.post(url, headers: headers, body: body.to_json).body
+
+    begin
+      download_url = JSON.parse(response)["url"]
+      if download_url
+        youtube_video.update_column(:download_url, download_url)
+      end
+    rescue
+      nil
+    end
+  end
+end
